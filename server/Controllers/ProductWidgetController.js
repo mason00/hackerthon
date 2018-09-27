0a59e9b255067f1ba73a7c155fa60dac05a83123
Mason Hu <Mason.Hu@zip.co>
2018-09-27 16:08:58 +1000

    mock data

--- server/Controllers/ProductWidgetController.js
+++ server/Controllers/ProductWidgetController.js
@@ -1,11 +1,41 @@
-const NewUserHtml = require('../Asserts/newUserHtml');
-const { LoggedInUserHtml } = require('../Asserts/loggedInUserHtml');
-const format = require("string-template");
-const escape = require("html-escape")
+const fetch = require('node-fetch');
 
-function GetWidgetData(req, res) {
-    const response = 'Hello';
+async function GetWidgetData(req, res) {
+    const merchantid = req.query && req.query.merchantid || '4f41a53e-1c44-4adf-9daf-117945cdf339';
+    const url = `https://api.zipmoney.com.au/v1/assets?merchantid=${merchantid}`;
+    
+    let response = null;
+    
+    await fetch(url)
+        .then(res => res.json())
+        .then(json => response = json);
+    
+    if (response) {
+        if (response.asset_values && response.asset_values.length) {
+            response.asset_values.push({
+                key: "{Price}",
+                value: getPrice(),
+            });
+        }
+        if (response.assets && response.assets.length) {
+            response.assets.push({
+                id: 3123,
+                type: "hackerthon",
+                sequence: 0,
+                content_type: "html",
+                url: null,
+                value: "https://www.oxfordshop.com.au/Customisation/Theme/Oxford/assets/images/zippay.png?v=2"
+            })
+        }
+    }
+
+    console.log(response);
     res.send(response);
+}
+
+function getPrice() {
+    const price = Math.floor(Math.random() * 2500) + 345;
+    return `${price}`;
 }
 
 module.exports = { GetWidgetData }
